37f88a465635108102e0da29f4604900029ac8c0
edward1wang <wangdali@gmail.com>
2017-08-15 14:43:54 -0400

    Create Update-Switches.py

---
+++ Update-Switches.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+""" Task: Update all switches with new port description commands
+
+
+"""
+
+# Function: Configure-Switch (#Switch-Number)
+
+    # If the switch needs SSH access: 
+    # Call CRASS.py with #Switch-Number and #Switch-Number.cfg (switch configuration command) 
+    # How to keep the existing switch port descriptions? 
+    
+    # If the switch needs Telnet (for Cat3524 and CatOS switches): 
+    # Call ? 
+    
+# End of Function: 
+
+# Function Update-Switches
+# Update all switches with new port description commands
+
+    # Get the switch list for updates
+    
+    # Process each switch #Switch-Number
+    
+        # Update each switch 
+        # Call function Confgiure-Switch(#Switch-Number)
+
+# End of Function: Update-Switches
+    
+    
